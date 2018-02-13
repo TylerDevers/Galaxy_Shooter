8030f2fb002919f5b65d7b24b242b1398bfa540b
TylerDevers <devers.tyler@gmail.com>
2018-02-13 10:31:07 -0800

    ship snaps to center and moves with wasd/arrow keys

--- Assets/2D Galaxy Assets/Scripts/Player.cs
+++ Assets/2D Galaxy Assets/Scripts/Player.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public float speed = 5f;
+
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = new Vector3(0f, 0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ShipMovement();
+	}
+
+	void ShipMovement()
+	{
+		float horizontalMove = Input.GetAxis("Horizontal");
+		float verticalMove = Input.GetAxis("Vertical");
+
+		transform.Translate(Vector3.right * speed * horizontalMove * Time.deltaTime);
+		transform.Translate(Vector3.up * speed * verticalMove * Time.deltaTime);
 	}
 }
