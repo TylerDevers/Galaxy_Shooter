5e1812400b47875df6cefb4e047f043b3ee1edd4
TylerDevers <devers.tyler@gmail.com>
2018-02-19 12:03:36 -0800

    added spanManager and SpawnManager.cs

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	[SerializeField] private GameObject _enemyShipPrefab;
+	[SerializeField] private GameObject[] _powerups;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(EnemySpawn());
+	}
+
+	IEnumerator EnemySpawn()
+	{
+		System.Random random = new System.Random();
+
+		while (true)
+		{
+			Instantiate(_enemyShipPrefab, new Vector3(random.Next(-75, 75)/10f, 6.5f, 0f), Quaternion.identity);
+			yield return new WaitForSeconds(5);
+		}
+	}
+	
+
+}
